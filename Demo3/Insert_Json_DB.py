779e50e2fe7f77d1dba8bc4cd1890814646f3b72
廖子慶 <liaoziqing@liaoziqingde-MacBook.local>
2018-02-02 10:33:12 +0800

    its OK

--- Demo3/Insert_Json_DB.py
+++ Demo3/Insert_Json_DB.py
@@ -1,9 +1,6 @@
 
 # coding: utf-8
 
-# In[14]:
-
-
 
 
 import pandas as pd
@@ -24,9 +21,6 @@ import math
 
 redis_ip = 'liaofxredis'
 r = redis.Redis(host=redis_ip,port=6379,db=0)
-#db connect
-
-#r = redis.Redis(host='localhost',port=6379,db=0)
 HOST = "liaofxpostgres"
 USER = 'postgres'
 PASSWORD = 'demois1207'
@@ -38,8 +32,8 @@ sql_conn = psycopg2.connect(database = DB,
 cur = sql_conn.cursor()
 
 
-# In[3]:
-#客群推薦的對照表有兩張，main_edition.json 是首頁的客群推薦對照表，second_edition是外匯頁的客群推薦對照表
+
+#客群推薦的對照表有兩張，main_edition.json 是首頁的客群推薦對照表同時被外匯使用，second_edition是首頁的客群推薦對照表的情境部分
 a = inspect.getfile(inspect.currentframe())
 b = os.path.dirname(os.path.abspath(a)) 
 file_path_main_edition = os.path.join( b , 'main_edition.json' )
@@ -48,77 +42,68 @@ main_Json = json.load(jsonfile)
 file_path_second_edition = os.path.join( b , 'second_edition.json' )
 jsonfile = open(file_path_second_edition,'r')
 second_Json = json.load(jsonfile)
+#Tag_Db，是整個Tag Server的資料，直接把一個時間點的資料彙出轉Tag_DB.json
 file_path_TagDB = os.path.join( b , 'TAG_DB.json' )
 jsonfile = open(file_path_TagDB,'r')
 TagDB_Json = json.load(jsonfile)
+#Offer身上的Offer Tag
 file_path_tag_offer = os.path.join( b , 'tag_offer.json' )
 jsonfile = open(file_path_tag_offer,'r')
 TagOffer_Json = json.load(jsonfile)
 
 
-# In[4]:
-
+#產製客群推薦對照表的存放表格位置
 SQL= 'CREATE TABLE main_edition ( id serial NOT NULL PRIMARY KEY , tag_info json NOT NULL )'
 cur.execute(SQL)
 sql_conn.commit()
 
-
-# In[4]:
-
+#產製客群推薦對照表2的存放表格位置
 SQL= 'CREATE TABLE second_edition ( id serial NOT NULL PRIMARY KEY , tag_info json NOT NULL )'
 cur.execute(SQL)
 sql_conn.commit()
 
 
-# In[5]:
-
+#TagServer的表格位置
 SQL= 'CREATE TABLE tag_db ( id serial NOT NULL PRIMARY KEY , tag_info json NOT NULL )'
 cur.execute(SQL)
 sql_conn.commit()
 
 
-# In[6]:
-
+#Offer & Offer Tag的表格位置
 SQL= 'CREATE TABLE offer_tagging ( id serial NOT NULL PRIMARY KEY , off_info json NOT NULL )'
 cur.execute(SQL)
 sql_conn.commit()
 
 
-# In[6]:
-
+#將客群推薦1的Json檔直存入
 for i in main_Json:
     SQL = "INSERT INTO main_edition(tag_info) VALUES('{}');".format(json.dumps(i))
     cur.execute(SQL)
     sql_conn.commit()
 
 
-# In[7]:
-
+#將客群推薦2的Json檔直接存入
 for i in second_Json:
     SQL = "INSERT INTO second_edition(tag_info) VALUES('{}');".format(json.dumps(i))
     cur.execute(SQL)
     sql_conn.commit()
 
 
-# In[8]:
-
+#將TAG_DB存入
 for i in TagDB_Json:
     SQL = "INSERT INTO tag_db(tag_info) VALUES('{}');".format(json.dumps(i))
     cur.execute(SQL)
     sql_conn.commit()
 
 
-# In[9]:
-
+#將Offer Tag存入
 for i in TagOffer_Json:
     SQL = "INSERT INTO offer_tagging(off_info) VALUES('{}');".format(json.dumps(i))
     cur.execute(SQL)
     sql_conn.commit()
 
 
-# In[12]:
-
-# 貼標庫格式
+#產生Tagging DB
 def Tag_query(tag, sql_db):
     SQL = "SELECT tag_info FROM tag_db WHERE tag_info->>'TAG_ID' = '{}';".format(tag)
     sql_db.execute(SQL)
@@ -134,14 +119,10 @@ def Tag_query(tag, sql_db):
     return data
 
 
-# In[11]:
-
-SQL= "Select tag_info from tag_db  where tag_info ->>'TAG_ID' = 'TG0001';"
-cur.execute(SQL)
-cur.fetchall()
 
 
-# In[16]:
+
+#讀取Tagging的資料，以下分別為一般的Tagging，與有Tag value的Tagging
 
 file_path = inspect.getfile(inspect.currentframe())
 file_direction = os.path.dirname(os.path.abspath(file_path))
@@ -151,8 +132,7 @@ df = pd.read_excel(tagging_log)
 df_value = pd.read_excel(tagging_value_log)
 
 
-# In[18]:
-
+#產生Tagging DB 
 def TaggingDB_ETL(data,data_value, TagDB=cur, TaggingDB=r):
     vids = data.ID.unique()
     for i in vids:
@@ -171,7 +151,6 @@ def TaggingDB_ETL(data,data_value, TagDB=cur, TaggingDB=r):
         TaggingDB.set(i, json.dumps(Tagging,ensure_ascii=False))
 
 
-# In[20]:
 
 TaggingDB_ETL(df,df_value,TagDB=cur,TaggingDB=r)
 
